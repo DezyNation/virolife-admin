ff415eeffe1ceba5b2fe203ff169de2eda47f634
DezyNation <87603422+DezyNation@users.noreply.github.com>
2023-08-03 00:39:52 +0530

    beneficiary details

--- src/app/dashboard/campaigns/view/[id]/page.jsx
+++ src/app/dashboard/campaigns/view/[id]/page.jsx
@@ -108,7 +108,7 @@ const CampaignInfo = ({ params }) => {
             bgColor={"blue.50"}
             rounded={"12"}
           >
-            {campaign.beneficiary_details != null || campaign.beneficiary_details != "null" ? (
+            {campaign.beneficiary_details != null && campaign.beneficiary_details != "null" ? (
               <>
                 This campaign will benefit{" "}
                 {JSON.parse(campaign?.beneficiary_details)?.name} of{" "}
