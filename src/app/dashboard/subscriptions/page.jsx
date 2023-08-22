222de3c3a43afd27a54676fca31322ad3a4844c8
DezyNation <87603422+DezyNation@users.noreply.github.com>
2023-08-22 20:01:52 +0530

    db trxn

--- src/app/dashboard/subscriptions/page.jsx
+++ src/app/dashboard/subscriptions/page.jsx
@@ -120,7 +120,7 @@ const page = () => {
                 <Td>{data?.user_id}</Td>
                 <Td>{data?.user_name}</Td>
                 <Td>{data?.parent_id}</Td>
-                <Td>{data?.name}</Td>
+                <Td>{data?.plan_name}</Td>
                 <Td>{data?.points}</Td>
                 <Td>{data?.created_at}</Td>
               </Tr>
