bd68bf6acddbdc487702ccbc7e5ff08044a99f51
DezyNation <87603422+DezyNation@users.noreply.github.com>
2023-09-01 23:39:26 +0530

    user register

--- src/app/dashboard/users/create/page.jsx
+++ src/app/dashboard/users/create/page.jsx
@@ -56,7 +56,7 @@ const Info = () => {
       distributor: "",
     },
     onSubmit: (values) => {
-      FormAxios.post(`/admin/user/register-user`, {
+      FormAxios.post(`/api/admin/user/register-user`, {
         ...values,
         address: JSON.stringify(addressObj),
         password_confirmation: Formik.values.password,
