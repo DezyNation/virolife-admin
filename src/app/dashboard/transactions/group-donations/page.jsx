8c540a13dfba1d7b10c19376493d3041ba2cc07c
DezyNation <87603422+DezyNation@users.noreply.github.com>
2023-09-05 18:58:53 +0530

    group donation fix

--- src/app/dashboard/transactions/group-donations/page.jsx
+++ src/app/dashboard/transactions/group-donations/page.jsx
@@ -16,71 +16,11 @@ import {
 } from "@chakra-ui/react";
 import BackendAxios from "@/utils/axios";
 
-const DonationTable = ({groupType}) => {
-  return(
-    <>
-<TableContainer height={"lg"} overflowY={"scroll"}>
-        <Table variant={"striped"} colorScheme="gray">
-          <Thead>
-            <Tr>
-              <Th>Trnxn ID</Th>
-              <Th>From</Th>
-              <Th>Description</Th>
-              <Th>Amount</Th>
-              <Th>Timestamp</Th>
-              <Th>Action</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {transactions
-              ?.filter((trnxn) => trnxn?.remarks?.includes(groupType))
-              .map((item, key) => (
-                <Tr key={key}>
-                  <Td>{item?.id}</Td>
-                  <Td>
-                    {item?.user?.name} ({item?.user?.id})
-                  </Td>
-                  <Td>{item?.remarks}</Td>
-                  <Td>₹ {item?.amount}</Td>
-                  <Td>{new Date(item?.created_at).toLocaleDateString()}</Td>
-                  <Td>
-                    {!item?.approved && !item?.deleted_at ? (
-                      <HStack>
-                        <Button
-                          size={"sm"}
-                          rounded={"full"}
-                          colorScheme="yellow"
-                          onClick={() => approveDonation(item.id)}
-                        >
-                          Confirm
-                        </Button>
-                        <Button
-                          size={"sm"}
-                          rounded={"full"}
-                          colorScheme="red"
-                          onClick={() => deleteDonation(item.id)}
-                        >
-                          Cancel
-                        </Button>
-                      </HStack>
-                    ) : null}
-                  </Td>
-                </Tr>
-              ))}
-          </Tbody>
-        </Table>
-      </TableContainer>
-    </>
-  )
-}
-
-const page = () => {
+const DonationTable = ({ groupType, transactions }) => {
   const [transactions, setTransactions] = useState([]);
   const Toast = useToast({
     position: "top-right",
   });
-  const [user, setUser] = useState("");
-
   function fetchMyCollections() {
     BackendAxios.get(`/api/admin/user-collections${user ? `/${user}` : ""}`)
       .then((res) => {
@@ -135,6 +75,65 @@ const page = () => {
         });
       });
   }
+  return (
+    <>
+      <TableContainer height={"lg"} overflowY={"scroll"}>
+        <Table variant={"striped"} colorScheme="gray">
+          <Thead>
+            <Tr>
+              <Th>Trnxn ID</Th>
+              <Th>From</Th>
+              <Th>Description</Th>
+              <Th>Amount</Th>
+              <Th>Timestamp</Th>
+              <Th>Action</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {transactions
+              ?.filter((trnxn) => trnxn?.remarks?.includes(groupType))
+              .map((item, key) => (
+                <Tr key={key}>
+                  <Td>{item?.id}</Td>
+                  <Td>
+                    {item?.user?.name} ({item?.user?.id})
+                  </Td>
+                  <Td>{item?.remarks}</Td>
+                  <Td>₹ {item?.amount}</Td>
+                  <Td>{new Date(item?.created_at).toLocaleDateString()}</Td>
+                  <Td>
+                    {!item?.approved && !item?.deleted_at ? (
+                      <HStack>
+                        <Button
+                          size={"sm"}
+                          rounded={"full"}
+                          colorScheme="yellow"
+                          onClick={() => approveDonation(item.id)}
+                        >
+                          Confirm
+                        </Button>
+                        <Button
+                          size={"sm"}
+                          rounded={"full"}
+                          colorScheme="red"
+                          onClick={() => deleteDonation(item.id)}
+                        >
+                          Cancel
+                        </Button>
+                      </HStack>
+                    ) : null}
+                  </Td>
+                </Tr>
+              ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+};
+
+const page = () => {
+  const [user, setUser] = useState("");
 
   return (
     <>
@@ -153,10 +152,12 @@ const page = () => {
       </HStack>
       <br />
       <Text fontWeight={"semibold"}>Primary Donations</Text>
-      <DonationTable groupType={'primary'} />
-      <br /><br /><br />
+      <DonationTable groupType={"primary"} />
+      <br />
+      <br />
+      <br />
       <Text fontWeight={"semibold"}>Secondary donation</Text>
-      <DonationTable groupType={'secondary'} />
+      <DonationTable groupType={"secondary"} />
     </>
   );
 };
